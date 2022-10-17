090be8794cc597e8d6fade4a96f5d5322a40d1b3
brooks <myt2000@126.com>
2022-10-17 15:43:04 +0800

    构造login和main的页面跳转，和发出广播

---
+++ app/src/main/java/com/brooks/broadcastbestpractice/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.brooks.broadcastbestpractice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityCollector.addActivity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ActivityCollector.removeActivity(this)
+    }
+}
---
+++ app/src/main/java/com/brooks/broadcastbestpractice/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.brooks.broadcastbestpractice
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.brooks.broadcastbestpractice.databinding.ActivityLoginBinding
+
+
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding.login.setOnClickListener {
+            val account = binding.accountEdit.text.toString()
+            val password = binding.passwordEdit.text.toString()
+            // 如果账号是admin且密码是123456，就认为登陆成功
+            if (account == "admin" && password == "123456") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else {
+                Toast.makeText(this, "account or password is invalid", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
