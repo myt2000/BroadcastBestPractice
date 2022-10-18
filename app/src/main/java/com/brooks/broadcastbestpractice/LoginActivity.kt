6428975f4a1c83fbadba11a997b871255da8e7ec
brooks <myt2000@126.com>
2022-10-18 09:38:19 +0800

    使用extentsions插件，修复bug让按键起作用

--- app/src/main/java/com/brooks/broadcastbestpractice/LoginActivity.kt
+++ app/src/main/java/com/brooks/broadcastbestpractice/LoginActivity.kt
@@ -2,18 +2,20 @@ package com.brooks.broadcastbestpractice
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import com.brooks.broadcastbestpractice.databinding.ActivityLoginBinding
+//import com.brooks.broadcastbestpractice.databinding.ActivityLoginBinding
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val binding = ActivityLoginBinding.inflate(layoutInflater)
-        binding.login.setOnClickListener {
-            val account = binding.accountEdit.text.toString()
-            val password = binding.passwordEdit.text.toString()
+        login.setOnClickListener {
+            val account = accountEdit.text.toString()
+            val password = passwordEdit.text.toString()
+            Log.d("账号和密码", "onCreate: $account $password  ")
             // 如果账号是admin且密码是123456，就认为登陆成功
             if (account == "admin" && password == "123456") {
                 val intent = Intent(this, MainActivity::class.java)
--- app/src/main/java/com/brooks/broadcastbestpractice/MainActivity.kt
+++ app/src/main/java/com/brooks/broadcastbestpractice/MainActivity.kt
@@ -2,15 +2,14 @@ package com.brooks.broadcastbestpractice
 
 import android.content.Intent
 import android.os.Bundle
-import com.brooks.broadcastbestpractice.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity: BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.forceOffline.setOnClickListener {
+        forceOffline.setOnClickListener {
             val intent = Intent("com.brooks.broadcastbestpractice.FORCE_OFFLINE")
             sendBroadcast(intent)
         }
